874a43e231e11d46e57f9adfc603bd27cd3e40a5
Patrick Maciel <patrickmaciel.info@gmail.com>
2024-11-18 18:15:17 -0300

    feat: first lambda function for AWS

---
+++ src/main/java/info/patrickmaciel/Main.java
@@ -0,0 +1,11 @@
+package info.patrickmaciel;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    for (String arg : args) {
+      System.out.println(arg);
+    }
+  }
+}
